b104cbd14e87779e1cda50a991c23c9fa6f48179
James Forrest <james@seslatero.au>
2023-06-03 10:50:59 +0930

    feat: add buttons and total

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,9 +5,27 @@ setBasePath(
   "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.4.0/dist/"
 ); // or copy stuff into public/ with a build task
 
-import { SlButton } from "@shoelace-style/shoelace/dist/react";
+import { SlButton, SlCard } from "@shoelace-style/shoelace/dist/react";
 
 import styles from "./app.module.scss";
+
+interface MenuItem {
+  name: string;
+  cost: number;
+}
+
+const MENU: MenuItem[] = [
+  { name: "Grind Coffee", cost: 0.5 },
+  { name: "Pod Coffee", cost: 1 },
+  { name: "Milk Week", cost: 1 },
+  { name: "Milk Shot", cost: 0.2 },
+  { name: "Chocolate", cost: 1.5 },
+  { name: "Can", cost: 1 },
+];
+
+function currency(value: number) {
+  return "$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
 
 const App = (): JSX.Element => {
   return (
@@ -17,10 +35,17 @@ const App = (): JSX.Element => {
         <h1 className={styles.headerTitle}>Bld42</h1>
       </header>
       <section className={styles.buttons}>
-        <SlButton>Click me</SlButton>
+        {MENU.map((menuItem, i) => (
+          <SlButton key={i} onClick={() => console.log(menuItem)}>
+            {menuItem.name} - {currency(menuItem.cost)}
+          </SlButton>
+        ))}
       </section>
-      <section className={styles.buttons}>
-        <SlButton>Click me</SlButton>
+      <section className={styles.total + " sl-theme-dark"}>
+        <SlCard>
+          <div slot="header">Total</div>
+          {currency(140.5)}
+        </SlCard>
       </section>
       <footer className={styles.footer}>Made for the Box Team</footer>
     </main>
